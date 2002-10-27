53d97485f836a809267da531e82f1491088deefb
tirsen <tirsen@3d9f0f3f-b372-0410-8897-e56e3f393856>
2002-10-27 12:13:18 +0000

    Some refactorings as I actually start understanding what I'm doing. Renamed some stuff into better names. AspectClass now extends AspectDefinition and can have target, interface and interceptor of its own. The interceptors of the AspectClass is added first to the interceptors of all the side-aspects.

--- nanning/src/test/com/tirsen/nanning/test/AspectClassTest.java
+++ nanning/src/test/com/tirsen/nanning/test/AspectClassTest.java
@@ -8,36 +8,34 @@ package com.tirsen.nanning.test;
 
 import junit.framework.TestCase;
 import com.tirsen.nanning.AspectClass;
-import com.tirsen.nanning.InterfaceDefinition;
+import com.tirsen.nanning.AspectDefinition;
 import com.tirsen.nanning.Aspects;
 import com.tirsen.nanning.Interceptor;
 
 /**
  * TODO document AspectClassTest
  *
- * <!-- $Id: AspectClassTest.java,v 1.2 2002-10-23 21:26:43 tirsen Exp $ -->
+ * <!-- $Id: AspectClassTest.java,v 1.3 2002-10-27 12:13:18 tirsen Exp $ -->
  *
  * @author $Author: tirsen $
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class AspectClassTest extends TestCase
 {
     public void testInterceptor() throws IllegalAccessException, InstantiationException, NoSuchMethodException
     {
-        AspectClass aspectClass = AspectClass.create();
-        InterfaceDefinition interfaceDefinition = new InterfaceDefinition();
-        interfaceDefinition.setInterface(Intf.class);
-        interfaceDefinition.addInterceptor(MockAspect.class);
-        interfaceDefinition.addInterceptor(MockAspect.class);
-        interfaceDefinition.setTarget(Impl.class);
-        aspectClass.addInterface(interfaceDefinition);
+        AspectClass aspectClass = new AspectClass();
+        aspectClass.setInterface(Intf.class);
+        aspectClass.addInterceptor(MockInterceptor.class);
+        aspectClass.addInterceptor(MockInterceptor.class);
+        aspectClass.setTarget(Impl.class);
 
         Intf intf = (Intf) aspectClass.newInstance();
         Impl impl = (Impl) Aspects.getTarget(intf, Intf.class);
 
         Interceptor[] interceptors = Aspects.getInterceptors(intf, Intf.class);
-        MockAspect aspect = (MockAspect) interceptors[0];
-        MockAspect aspect2 = (MockAspect) interceptors[1];
+        MockInterceptor aspect = (MockInterceptor) interceptors[0];
+        MockInterceptor aspect2 = (MockInterceptor) interceptors[1];
 
         aspect.expectTarget(impl);
         aspect.expectProxy(intf);
@@ -52,78 +50,61 @@ public class AspectClassTest extends TestCase
         aspect2.verify();
     }
 
-    public void testAspectsOnProxy() throws InstantiationException, IllegalAccessException
+    public void testSideAspectAndAspectsOnProxy() throws IllegalAccessException, InstantiationException, NoSuchMethodException
     {
-        AspectClass aspectClass = AspectClass.create();
-        InterfaceDefinition interfaceDefinition = new InterfaceDefinition();
-        interfaceDefinition.setInterface(Intf.class);
-        interfaceDefinition.addInterceptor(MockAspect.class);
-        interfaceDefinition.setTarget(Impl.class);
-        aspectClass.addInterface(interfaceDefinition);
-        aspectClass.addInterceptor(MockAspect.class);
-
-        Intf proxy = (Intf) aspectClass.newInstance();
-
-        assertEquals(2, Aspects.getInterceptors(proxy, Intf.class).length);
-        MockAspect intfInterceptor = (MockAspect) Aspects.getInterceptors(proxy, Intf.class)[0];
-        MockAspect proxyInterceptor = (MockAspect) Aspects.getInterceptors(proxy)[0];
-
-        intfInterceptor.expectCalledTimes(1);
-        intfInterceptor.expectProxy(proxy);
-        proxyInterceptor.expectProxy(proxy);
-
-        proxy.call();
-
-        intfInterceptor.verify();
-        proxyInterceptor.verify();
-    }
-
-    public void testSideAspect() throws IllegalAccessException, InstantiationException, NoSuchMethodException
-    {
-        AspectClass aspectClass = AspectClass.create();
-        InterfaceDefinition interfaceDefinition = new InterfaceDefinition();
-        interfaceDefinition.setInterface(Intf.class);
-        interfaceDefinition.addInterceptor(MockAspect.class);
-        interfaceDefinition.addInterceptor(NullAspect.class);
-        interfaceDefinition.setTarget(Impl.class);
-        aspectClass.addInterface(interfaceDefinition);
-        interfaceDefinition = new InterfaceDefinition();
+        AspectClass aspectClass = new AspectClass();
+        aspectClass.setInterface(Intf.class);
+        aspectClass.addInterceptor(MockInterceptor.class);
+        aspectClass.addInterceptor(NullInterceptor.class);
+        aspectClass.setTarget(Impl.class);
+        AspectDefinition interfaceDefinition = new AspectDefinition();
         interfaceDefinition.setInterface(SideAspect.class);
-        interfaceDefinition.addInterceptor(NullAspect.class);
-        interfaceDefinition.addInterceptor(MockAspect.class);
+        interfaceDefinition.addInterceptor(NullInterceptor.class);
+        interfaceDefinition.addInterceptor(MockInterceptor.class);
         interfaceDefinition.setTarget(SideAspectImpl.class);
-        aspectClass.addInterface(interfaceDefinition);
+        aspectClass.addSideAspect(interfaceDefinition);
 
         Object bigMomma = aspectClass.newInstance();
 
-        Impl impl = (Impl) Aspects.getTarget(bigMomma, Intf.class);
-        MockAspect checkIntf = (MockAspect) (Aspects.getInterceptors(bigMomma, Intf.class)[0]);
-        checkIntf.expectProxy(bigMomma);
-        checkIntf.expectMethod(Intf.class.getMethod("call", null));
-        checkIntf.expectTarget(impl);
+        Impl target = (Impl) Aspects.getTarget(bigMomma, Intf.class);
+        MockInterceptor classInterceptor = (MockInterceptor) (Aspects.getInterceptors(bigMomma, Intf.class)[0]);
+        classInterceptor.setName("classInterceptor");
+        classInterceptor.expectAtIndex(0);
+        classInterceptor.expectNumberOfInterceptors(2);
+        classInterceptor.expectCalledTimes(2);
+        classInterceptor.expectProxy(bigMomma);
+        classInterceptor.expectMethod(Intf.class.getMethod("call", null));
+        classInterceptor.expectTarget(target);
 
-        SideAspectImpl sideAspectImpl = (SideAspectImpl) Aspects.getTarget(bigMomma, SideAspect.class);
-        MockAspect checkSide = (MockAspect) (Aspects.getInterceptors(bigMomma, SideAspect.class)[1]);
-        checkSide.expectProxy(bigMomma);
-        checkSide.expectMethod(SideAspect.class.getMethod("sideCall", null));
-        checkSide.expectTarget(sideAspectImpl);
+        SideAspectImpl sideTarget = (SideAspectImpl) Aspects.getTarget(bigMomma, SideAspect.class);
+        MockInterceptor sideInterceptor = (MockInterceptor) (Aspects.getInterceptors(bigMomma, SideAspect.class)[3]);
+        sideInterceptor.setName("sideInterceptor");
+        sideInterceptor.expectAtIndex(3);
+        sideInterceptor.expectNumberOfInterceptors(4);
+        sideInterceptor.expectCalledTimes(1);
+        sideInterceptor.expectProxy(bigMomma);
+        sideInterceptor.expectMethod(SideAspect.class.getMethod("sideCall", null));
+        sideInterceptor.expectTarget(sideTarget);
 
+        // this calls the class-target and the class-interceptor
         ((Intf) bigMomma).call();
+        // this calls the side-target, the class-interceptor and the side-interceptor
+        classInterceptor.expectTarget(null);
+        classInterceptor.expectMethod(null);
+        classInterceptor.expectNumberOfInterceptors(4);
         ((SideAspect) bigMomma).sideCall();
 
-        checkIntf.verify();
-        impl.verify();
-        checkSide.verify();
-        sideAspectImpl.verify();
+        classInterceptor.verify();
+        target.verify();
+        sideInterceptor.verify();
+        sideTarget.verify();
     }
 
     public void testNoAspects() throws IllegalAccessException, InstantiationException
     {
-        AspectClass aspectClass = AspectClass.create();
-        InterfaceDefinition interfaceDefinition = new InterfaceDefinition();
-        interfaceDefinition.setInterface(Intf.class);
-        interfaceDefinition.setTarget(Impl.class);
-        aspectClass.addInterface(interfaceDefinition);
+        AspectClass aspectClass = new AspectClass();
+        aspectClass.setInterface(Intf.class);
+        aspectClass.setTarget(Impl.class);
         Intf intf = (Intf) aspectClass.newInstance();
 
         Impl impl = (Impl) Aspects.getTarget(intf, Intf.class);
---
+++ nanning/src/test/com/tirsen/nanning/test/TestConfig.java
@@ -0,0 +1,24 @@
+/*
+ * Nanning Aspects
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package com.tirsen.nanning.test;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO document TestConfig
+ *
+ * <!-- $Id: TestConfig.java,v 1.1 2002-10-27 12:13:18 tirsen Exp $ -->
+ *
+ * @author $Author: tirsen $
+ * @version $Revision: 1.1 $
+ */
+public class TestConfig extends TestCase
+{
+    public void testConfig()
+    {
+    }
+}
