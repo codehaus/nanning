7248cc764221cb008f253cf549b9c9609373866a
tirsen <tirsen@3d9f0f3f-b372-0410-8897-e56e3f393856>
2002-11-05 20:47:06 +0000

    Implemented stateless interceptors.

---
+++ trunk/nanning/src/main/com/tirsen/nanning/StatelessInterceptor.java
@@ -0,0 +1,20 @@
+/*
+ * Nanning Aspects
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package com.tirsen.nanning;
+
+/**
+ * This is a marker-interface to indicate that an interceptor does not keep state and one instance can be used
+ * as the interceptor for all aspect-instances.
+ *
+ * <!-- $Id: StatelessInterceptor.java,v 1.1 2002-11-05 20:47:06 tirsen Exp $ -->
+ *
+ * @author $Author: tirsen $
+ * @version $Revision: 1.1 $
+ */
+public interface StatelessInterceptor
+{
+}
--- trunk/nanning/src/test/com/tirsen/nanning/test/NullInterceptor.java
+++ trunk/nanning/src/test/com/tirsen/nanning/test/NullInterceptor.java
@@ -8,16 +8,17 @@ package com.tirsen.nanning.test;
 
 import com.tirsen.nanning.Interceptor;
 import com.tirsen.nanning.Invocation;
+import com.tirsen.nanning.StatelessInterceptor;
 
 /**
  * TODO document NullInterceptor
  *
- * <!-- $Id: NullInterceptor.java,v 1.1 2002-10-27 12:13:18 tirsen Exp $ -->
+ * <!-- $Id: NullInterceptor.java,v 1.2 2002-11-05 20:46:39 tirsen Exp $ -->
  *
  * @author $Author: tirsen $
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
-public class NullInterceptor implements Interceptor
+public class NullInterceptor implements Interceptor, StatelessInterceptor
 {
     public Object invoke(Invocation invocation) throws Throwable
     {
