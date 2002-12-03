8a38c05959c3e2ff11b3b6a8aeaab8152ca1ba61
lecando <lecando@3d9f0f3f-b372-0410-8897-e56e3f393856>
2002-12-03 13:38:46 +0000

    *** empty log message ***

---
+++ nanning/src/main/com/tirsen/nanning/samples/CacheInterceptor.java
@@ -0,0 +1,52 @@
+package com.tirsen.nanning.samples;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.tirsen.nanning.Attributes;
+import com.tirsen.nanning.FilterMethodsInterceptor;
+import com.tirsen.nanning.Invocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class CacheInterceptor implements FilterMethodsInterceptor {
+    private static final Log logger = LogFactory.getLog(CacheInterceptor.class);
+
+    /**
+     * TODO Should be an LRU-cache
+     */
+    private Map cache = new HashMap();
+
+    public boolean interceptsMethod(Method method) {
+        return Attributes.hasAttribute(method, "cache");
+    }
+
+    public Object invoke(Invocation invocation) throws Throwable {
+        Method method = invocation.getMethod();
+        Map methodCache = getMethodCache(method);
+        List args = Arrays.asList(invocation.getArgs()); // needs args as collection for hashCode and equals
+        Object cachedResult = methodCache.get(args);
+        if(cachedResult == null) {
+            logger.debug("caching call for method " + method + " with args " + args);
+            cachedResult = invocation.invokeNext();
+            methodCache.put(args, cachedResult);
+        }
+        return cachedResult;
+    }
+
+    private Map getMethodCache(Method method) {
+        Map methodCache = (Map) cache.get(method);
+        if(methodCache == null) {
+            methodCache = new HashMap();
+            cache.put(method, methodCache);
+        }
+        return methodCache;
+    }
+
+    public void clearCache() {
+        cache.clear();
+    }
+}
