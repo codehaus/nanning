ec21fde372e4eb9a9b8698aafba7c37e1bc61d4c
tirsen <tirsen@3d9f0f3f-b372-0410-8897-e56e3f393856>
2002-12-04 07:51:04 +0000

    *** empty log message ***

---
+++ trunk/nanning/src/main/com/tirsen/nanning/samples/prevayler/Identity.java
@@ -0,0 +1,10 @@
+package com.tirsen.nanning.samples.prevayler;
+
+/**
+ * TODO document Identity
+ *
+ * @author <a href="mailto:jon_tirsen@yahoo.com">Jon Tirsén</a>
+ * @version $Revision: 1.1 $
+ */
+public class Identity {
+}
