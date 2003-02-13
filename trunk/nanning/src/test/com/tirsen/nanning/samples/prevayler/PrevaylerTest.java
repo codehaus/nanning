826af9b20e425918de5743d5cbf5dcc5fe90df2c
lecando <lecando@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-02-13 14:21:33 +0000

    Fixed some bugs and added CountingPrevayler.

--- trunk/nanning/src/test/com/tirsen/nanning/samples/prevayler/PrevaylerTest.java
+++ trunk/nanning/src/test/com/tirsen/nanning/samples/prevayler/PrevaylerTest.java
@@ -180,32 +180,6 @@ public class PrevaylerTest extends AbstractAttributesTest {
         return (MySystem) CurrentPrevayler.getSystem();
     }
 
-    private static class CountingPrevayler extends GarbageCollectingPrevayler {
-        private int numberOfCommandsInLog = 0;
-
-        public CountingPrevayler(IdentifyingSystem system, String dir) throws IOException, ClassNotFoundException {
-            super(system, dir);
-        }
-
-        public Serializable executeCommand(Command command) throws Exception {
-            numberOfCommandsInLog++;
-            return super.executeCommand(command);
-        }
-
-        public void takeSnapshot() throws IOException {
-            numberOfCommandsInLog = 0;
-            super.takeSnapshot();
-        }
-
-        public void assertNumberOfCommands(String message, int expectedNumber) {
-            assertEquals(message + ", wrong number of commands in log", expectedNumber, numberOfCommandsInLog);
-        }
-
-        public void assertNumberOfCommands(int expectedNumber) {
-            assertEquals("wrong number of commands in log", expectedNumber, numberOfCommandsInLog);
-        }
-    }
-
     private CountingPrevayler newPrevayler() throws IOException, ClassNotFoundException {
         CountingPrevayler prevayler = new CountingPrevayler((IdentifyingSystem) aspectFactory.newInstance(MySystem.class),
                                 prevaylerDir.getAbsolutePath());
