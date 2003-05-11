80a3ca35bbba455f6765278395e759c4e36dfa41
tirsen <tirsen@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-05-11 14:50:48 +0000

    Reimplemented Pointcuts and InterceptorAspect, added Profiler, thanx to Khalil Bouhamza!

---
+++ nanning/src/test/com/tirsen/nanning/config/AttributesTestClass.java
@@ -0,0 +1,10 @@
+package com.tirsen.nanning.config;
+
+public interface AttributesTestClass {
+    /**
+     * @attribute
+     */
+    void methodWithAttribute();
+
+    void methodWithoutAttribute();
+}
