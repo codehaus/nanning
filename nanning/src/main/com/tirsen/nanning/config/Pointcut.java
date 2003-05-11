0db00220e8f1f7a4971828de10055bfe6dc563aa
tirsen <tirsen@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-05-11 14:49:15 +0000

    Reimplemented Pointcuts and InterceptorAspect, added Profiler, thanx to Khalil Bouhamza!

---
+++ nanning/src/main/com/tirsen/nanning/config/Pointcut.java
@@ -0,0 +1,16 @@
+package com.tirsen.nanning.config;
+
+import com.tirsen.nanning.MixinInstance;
+import com.tirsen.nanning.AspectInstance;
+
+import java.util.Iterator;
+import java.lang.reflect.Method;
+
+public interface Pointcut {
+    /**
+     * Determine methods to advise for this mixin.
+     * @param mixin
+     * @return Methods to advise.
+     */
+    Method[] methodsToAdvise(AspectInstance instance, MixinInstance mixin);
+}
