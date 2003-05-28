77444910d6bbdf8fd60d81c27aeda37d563edcde
lecando <lecando@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-05-28 13:43:48 +0000

    Updated prevayler and stuff

--- trunk/nanning/src/main/com/tirsen/nanning/samples/prevayler/CountingPrevayler.java
+++ trunk/nanning/src/main/com/tirsen/nanning/samples/prevayler/CountingPrevayler.java
@@ -7,30 +7,50 @@ import org.prevayler.Prevayler;
 import org.prevayler.Transaction;
 
 public class CountingPrevayler extends FilterPrevayler {
-    private int numberOfCommandsInLog = 0;
+    private int numberOfTransactionsInLog = 0;
+    private StringBuffer transactionLog;
 
     public CountingPrevayler(Prevayler prevayler) {
         super(prevayler);
     }
 
+    public void setLogTransactionMethods(boolean flag) {
+        transactionLog = flag ? new StringBuffer() : null;
+    }
+
     public void execute(Transaction transaction) {
-        numberOfCommandsInLog++;
+        numberOfTransactionsInLog++;
+        if (transactionLog != null && transaction instanceof InvokeCommand) {
+            InvokeCommand invokeCommand = (InvokeCommand) transaction;
+            if (transactionLog.length() != 0) {
+                transactionLog.append(" ");
+            }
+            transactionLog.append(invokeCommand.getCall().getMethod().getName());
+        }
+
         super.execute(transaction);
     }
 
     public void assertNumberOfCommands(String message, int expectedNumber) {
-        Assert.assertEquals(message + ", wrong number of commands in log", expectedNumber, numberOfCommandsInLog);
+        Assert.assertEquals(message + ", wrong number of commands in log", expectedNumber, numberOfTransactionsInLog);
     }
 
     public void assertNumberOfCommands(int expectedNumber) {
-        Assert.assertEquals("wrong number of commands in log", expectedNumber, numberOfCommandsInLog);
+        Assert.assertEquals("wrong number of commands in log", expectedNumber, numberOfTransactionsInLog);
     }
 
-    public int getNumberOfCommandsInLog() {
-        return numberOfCommandsInLog;
+    public int getNumberOfTransactionsInLog() {
+        return numberOfTransactionsInLog;
     }
-    
-    public void resetNumberOfCommands() {
-        numberOfCommandsInLog = 0;
+
+    public void resetCount() {
+        numberOfTransactionsInLog = 0;
+        if (transactionLog != null) {
+            transactionLog = new StringBuffer();
+        }
+    }
+
+    public String getTransactionLog() {
+        return transactionLog.toString();
     }
 }
