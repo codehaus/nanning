1e8f57abffc0863825a670b193b69ad1fb1e8266
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-06-16 09:55:21 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1563'.

---
+++ tags/BUILD_1563/nanning/src/main/com/tirsen/nanning/samples/prevayler/CheckTransactionUnsupportedInterceptor.java
@@ -0,0 +1,34 @@
+package com.tirsen.nanning.samples.prevayler;
+
+import java.lang.reflect.Method;
+
+import com.tirsen.nanning.*;
+import com.tirsen.nanning.attribute.Attributes;
+
+public class CheckTransactionUnsupportedInterceptor implements MethodInterceptor, FilterMethodsInterceptor {
+    private static ThreadLocal transactionsUnsupported = new ThreadLocal();
+
+    static boolean isTransactionsSupported() {
+        return transactionsUnsupported.get() == null;
+    }
+
+    static void enterTransactionsUnsupported() {
+        transactionsUnsupported.set(transactionsUnsupported);
+    }
+
+    static void exitTransactionsUnsupported() {
+        transactionsUnsupported.set(null);
+    }
+
+    public Object invoke(Invocation invocation) throws Throwable {
+        if (!isTransactionsSupported()) {
+            throw new IllegalStateException("Transactions are not supported in the current calling context.");
+        }
+
+        return invocation.invokeNext();
+    }
+
+    public boolean interceptsMethod(Method method) {
+        return Attributes.hasAttribute(method, "transaction");
+    }
+}
