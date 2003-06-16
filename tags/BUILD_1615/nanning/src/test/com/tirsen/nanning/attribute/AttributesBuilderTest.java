0c7a82572b46adde315df94cf725533b675ca58c
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-06-16 15:25:46 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1615'.

---
+++ tags/BUILD_1615/nanning/src/test/com/tirsen/nanning/attribute/AttributesBuilderTest.java
@@ -0,0 +1,70 @@
+package com.tirsen.nanning.attribute;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import com.thoughtworks.qdox.parser.impl.JFlexLexer;
+import com.thoughtworks.qdox.parser.impl.Parser;
+import junit.framework.TestCase;
+
+public class AttributesBuilderTest extends TestCase {
+    private List classPropertiesHelpers;
+    private AttributesBuilder builder;
+    private FileInputStream input;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        builder = new AttributesBuilder();
+        File javaFile = AbstractAttributesTest.findNanningFile("src" + File.separator +
+                                                               "test" + File.separator +
+                                                               "com" + File.separator +
+                                                               "tirsen" + File.separator +
+                                                               "nanning" + File.separator +
+                                                               "attribute" + File.separator +
+                                                               "AttributesTestClass.java");
+
+        input = new FileInputStream(javaFile);
+        new Parser(new JFlexLexer(input), builder).parse();
+
+        classPropertiesHelpers = builder.getClassPropertiesHelpers();
+        assertEquals(2, classPropertiesHelpers.size());
+    }
+
+    protected void tearDown() throws Exception {
+        input.close();
+
+        super.tearDown();
+    }
+
+    public void testOuterClass() throws IOException {
+        ClassPropertiesHelper classPropertiesHelper = (ClassPropertiesHelper) classPropertiesHelpers.get(0);
+        assertEquals("AttributesTestClass", classPropertiesHelper.getClassName());
+        assertEquals("com.tirsen.nanning.attribute", classPropertiesHelper.getPackageName());
+
+        ClassAttributes attributes = new ClassAttributes(AttributesTestClass.class);
+        classPropertiesHelper.setTargetClassAttributes(attributes);
+        classPropertiesHelper.transferAttributesToTarget();
+
+        assertEquals(AttributesTest.FIELD_VALUE,
+                     attributes.getAttribute(AttributesTest.field, AttributesTest.FIELD_ATTRIBUTE));
+        assertEquals(AttributesTest.METHOD_VALUE,
+                     attributes.getAttribute(AttributesTest.method, AttributesTest.METHOD_ATTRIBUTE));
+    }
+
+    public void testInnerClass() throws IOException {
+        ClassPropertiesHelper classPropertiesHelper = (ClassPropertiesHelper) classPropertiesHelpers.get(1);
+        assertEquals("AttributesTestClass$InnerClass", classPropertiesHelper.getClassName());
+        assertEquals("com.tirsen.nanning.attribute", classPropertiesHelper.getPackageName());
+
+        ClassAttributes attributes = new ClassAttributes(AttributesTestClass.InnerClass.class);
+        classPropertiesHelper.setTargetClassAttributes(attributes);
+        classPropertiesHelper.transferAttributesToTarget();
+
+        assertEquals(AttributesTest.INNER_VALUE, attributes.getAttribute(AttributesTest.INNER_ATTRIBUTE));
+        assertEquals(AttributesTest.INNER_FIELD_VALUE, attributes.getAttribute(AttributesTest.innerField, AttributesTest.INNER_FIELD_ATTRIBUTE));
+    }
+
+}
---
+++ tags/BUILD_1615/nanning/src/test/com/tirsen/nanning/config/ClassIntroductorTest.java
@@ -0,0 +1,25 @@
+package com.tirsen.nanning.config;
+
+import junit.framework.TestCase;
+import com.tirsen.nanning.AspectInstance;
+
+public class ClassIntroductorTest extends TestCase {
+    public static interface Interface {}
+    public static class Implementation implements Interface {}
+
+    public static interface OtherInterface {}
+    
+    public void testIntroduce() {
+        ClassIntroductor introductor = new ClassIntroductor(Interface.class, Implementation.class);
+        AspectInstance aspectInstance = new AspectInstance(Interface.class);
+        introductor.introduce(aspectInstance);
+        assertEquals(1, aspectInstance.getMixins().size());
+    }
+    
+    public void testNotIntroduce() {
+        ClassIntroductor introductor = new ClassIntroductor(Interface.class, Implementation.class);
+        AspectInstance aspectInstance = new AspectInstance(OtherInterface.class);
+        introductor.introduce(aspectInstance);
+        assertEquals(0, aspectInstance.getMixins().size());
+    }
+}
