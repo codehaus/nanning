a173f54e72c80cbdb57805a0333e9bbd2791edc7
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-06-16 15:25:43 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1607'.

---
+++ tags/BUILD_1607/nanning/src/main/com/tirsen/nanning/samples/rmi/RemoteMarshaller.java
@@ -0,0 +1,125 @@
+package com.tirsen.nanning.samples.rmi;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.tirsen.nanning.AspectInstance;
+import com.tirsen.nanning.Aspects;
+import com.tirsen.nanning.MixinInstance;
+import com.tirsen.nanning.attribute.Attributes;
+import com.tirsen.nanning.config.Aspect;
+import com.tirsen.nanning.config.AspectSystem;
+import com.tirsen.nanning.samples.prevayler.Identity;
+import com.tirsen.nanning.samples.prevayler.Marshaller;
+
+public class RemoteMarshaller implements Marshaller {
+    private ObjectTable objectTable = new ObjectTable();
+    private AspectSystem aspectSystem;
+    private ServerConnectionManager connectionManager;
+
+    public static RemoteMarshaller createClientSideMarshaller() {
+        AspectSystem aspectSystem = new AspectSystem();
+
+        aspectSystem.addAspect(new Aspect() {
+            public void adviseMixin(AspectInstance aspectInstance, MixinInstance mixin) {
+            }
+
+            public void advise(AspectInstance aspectInstance) {
+            }
+
+            public void introduce(AspectInstance aspectInstance) {
+                MixinInstance mixinInstance = new MixinInstance();
+                mixinInstance.setInterfaceClass(aspectInstance.getClassIdentifier());
+                aspectInstance.addMixin(mixinInstance);
+            }
+        });
+
+        RemoteMarshaller remoteMarshaller = new RemoteMarshaller();
+
+        RemoteAspect aspect = new RemoteAspect();
+        aspect.setMarshaller(remoteMarshaller);
+        aspectSystem.addAspect(aspect);
+        remoteMarshaller.aspectSystem = aspectSystem;
+
+        return remoteMarshaller;
+    }
+
+    public static RemoteMarshaller createServerSideMarshaller(ServerConnectionManager connectionManager) {
+        RemoteMarshaller remoteMarshaller = new RemoteMarshaller();
+        remoteMarshaller.connectionManager = connectionManager;
+        return remoteMarshaller;
+    }
+
+
+    private RemoteMarshaller() {
+    }
+
+    public Object unmarshal(Object o) {
+        if (o instanceof Identity) {
+            Identity identity = (Identity) o;
+            Object id = identity.getIdentifier();
+            if (!objectTable.isIDRegistered(id)) {
+                if (isServerSide()) {
+                    // is remote and stub has not been created
+                    throw new RuntimeException("Did not find remote object on the server side (timeout?): " + o);
+                }
+                Class objectClass = identity.getObjectClass();
+                Object stub = aspectSystem.newInstance(objectClass);
+                Aspects.getAspectInstance(stub).setTarget(objectClass, identity);
+                return stub;
+            } else {
+                // is local or stub has already been created
+                return objectTable.getFromID(id);
+            }
+        }
+        return o;
+    }
+
+    private boolean isServerSide() {
+        return aspectSystem == null;
+    }
+
+    public Object marshal(Object o) {
+        if (o == null) {
+            return null;
+        }
+
+        if (Attributes.hasInheritedAttribute(o.getClass(), "remote")) {
+            if (isRemoteStub(o)) {
+                return getSingleMixinTarget(o);
+            } else {
+                return new RemoteIdentity(Aspects.getAspectInstance(o).getClassIdentifier(), registerID(o),
+                                          connectionManager);
+            }
+        } else {
+            assert Aspects.isAspectObject(o) ? !isRemoteStub(o) : true
+                    : o + " was remote stub but did not have 'remote'-attribute";
+            assert !(o instanceof RemoteIdentity); 
+        }
+
+        return o;
+    }
+
+    Object registerID(Object o) {
+        return objectTable.register(o);
+    }
+
+    public static boolean isRemoteStub(Object o) {
+        return getSingleMixinTarget(o) instanceof RemoteIdentity;
+    }
+
+    private static Object getSingleMixinTarget(Object o) {
+        Collection mixins = Aspects.getAspectInstance(o).getMixins();
+        Iterator iterator = mixins.iterator();
+        assert iterator.hasNext() : o + " doesn't have any mixins";
+        MixinInstance mixinInstance = (MixinInstance) iterator.next();
+        assert !iterator.hasNext() : "don't support several mixins";
+
+        Object target = mixinInstance.getTarget();
+        return target;
+    }
+
+    public void reset() {
+        objectTable.clear();
+    }
+}
---
+++ tags/BUILD_1607/nanning/src/test/com/tirsen/nanning/config/ClassAspectTest.java
@@ -0,0 +1,57 @@
+package com.tirsen.nanning.config;
+
+import junit.framework.TestCase;
+import com.tirsen.nanning.AspectInstance;
+import com.tirsen.nanning.MixinInstance;
+
+public class ClassAspectTest extends TestCase {
+    private boolean wasCalled;
+
+    public static interface Interface {}
+
+    public static interface OtherInterface {}
+
+    public void testAdvise() {
+        ClassAspect classAspect = new ClassAspect(Interface.class);
+        classAspect.addAspect(new Aspect() {
+            public void introduce(AspectInstance aspectInstance) {
+            }
+
+            public void adviseMixin(AspectInstance aspectInstance, MixinInstance mixin) {
+            }
+
+            public void advise(AspectInstance aspectInstance) {
+                wasCalled = true;
+            }
+        });
+
+        assertFalse(wasCalled);
+        classAspect.advise(new AspectInstance(OtherInterface.class));
+        assertFalse(wasCalled);
+        classAspect.advise(new AspectInstance(Interface.class));
+        assertTrue(wasCalled);
+    }
+
+    public void testIntroduce() {
+        ClassAspect classAspect = new ClassAspect(Interface.class);
+        classAspect.addAspect(new Aspect() {
+            public void introduce(AspectInstance aspectInstance) {
+                wasCalled = true;
+            }
+
+            public void adviseMixin(AspectInstance aspectInstance, MixinInstance mixin) {
+            }
+
+            public void advise(AspectInstance aspectInstance) {
+            }
+        });
+
+        assertFalse(wasCalled);
+        classAspect.newInstance(OtherInterface.class);
+        assertFalse(wasCalled);
+        classAspect.newInstance(Interface.class);
+        assertTrue(wasCalled);
+    }
+
+
+}
