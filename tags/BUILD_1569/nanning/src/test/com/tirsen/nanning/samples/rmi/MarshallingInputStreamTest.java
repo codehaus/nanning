6f900883410aa7056ed3ec4484a162945337430d
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-06-16 09:55:24 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1569'.

---
+++ tags/BUILD_1569/nanning/src/test/com/tirsen/nanning/samples/rmi/MarshallingInputStreamTest.java
@@ -0,0 +1,59 @@
+package com.tirsen.nanning.samples.rmi;
+
+import junit.framework.TestCase;
+import com.tirsen.nanning.samples.prevayler.Marshaller;
+
+import java.io.*;
+
+public class MarshallingInputStreamTest extends TestCase {
+    private ByteArrayOutputStream bytes;
+    private Marshaller marshaller;
+    private ObjectOutputStream out;
+
+    public static class DontMarshalObject implements Serializable {
+    }
+
+    public static class MarshalObject implements Serializable {
+    }
+
+    public static class MarshalledObject implements Serializable {
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        bytes = new ByteArrayOutputStream();
+        marshaller = new Marshaller() {
+            public Object marshal(Object o) {
+                fail();
+                return null;
+            }
+
+            public Object unmarshal(Object o) {
+                if (o instanceof MarshalledObject) {
+                    return new MarshalObject();
+                } else {
+                    return o;
+                }
+            }
+        };
+
+        out = new ObjectOutputStream(bytes);
+    }
+
+    public void testDontMarshalObject() throws IOException, ClassNotFoundException {
+        out.writeObject(new DontMarshalObject());
+
+        MarshallingInputStream in = new MarshallingInputStream(new ByteArrayInputStream(bytes.toByteArray()), marshaller);
+        Object readObject = in.readObject();
+        assertTrue(readObject instanceof DontMarshalObject);
+    }
+
+    public void testMarshalObject() throws IOException, ClassNotFoundException {
+        out.writeObject(new MarshalledObject());
+
+        MarshallingInputStream in = new MarshallingInputStream(new ByteArrayInputStream(bytes.toByteArray()), marshaller);
+        Object readObject = in.readObject();
+        assertTrue(readObject instanceof MarshalObject);
+    }
+}
