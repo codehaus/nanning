e9d58dbb7db3c78a5fb0b676983f85aa72af54d3
lecando <lecando@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-07-01 06:22:07 +0000

    reenabled tagging

--- trunk/nanning/src/main/com/tirsen/nanning/samples/prevayler/TestSystem.java
+++ trunk/nanning/src/main/com/tirsen/nanning/samples/prevayler/TestSystem.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestSystem extends AbstractClockedSystem {
+    static final long serialVersionUID = -8179360042523144918L;
+    
     public List list1 = new ArrayList();
     public List list2 = new ArrayList();
 }
