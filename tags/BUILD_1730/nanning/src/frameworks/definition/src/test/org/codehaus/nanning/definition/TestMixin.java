581093436d6477f347b5b555e1af91bf3a76488b
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-07-04 13:05:29 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1730'.

---
+++ tags/BUILD_1730/nanning/src/frameworks/definition/src/test/org/codehaus/nanning/definition/TestMixin.java
@@ -0,0 +1,19 @@
+/*
+ * Nanning Aspects
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.codehaus.nanning.definition;
+
+/**
+ * TODO document TestMixin
+ *
+ * <!-- $Id: TestMixin.java,v 1.1 2003-07-04 10:53:57 lecando Exp $ -->
+ *
+ * @author $Author: lecando $
+ * @version $Revision: 1.1 $
+ */
+public interface TestMixin {
+    void mixinCall();
+}
