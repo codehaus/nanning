50e27729731712652e0b52eeea20e808b9da79df
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-07-04 13:05:26 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1727'.

---
+++ tags/BUILD_1727/nanning/src/frameworks/prevayler/src/test/org/codehaus/nanning/prevayler/SoftMapTest.java
@@ -0,0 +1,122 @@
+package org.codehaus.nanning.prevayler;
+
+import java.io.*;
+
+import junit.framework.TestCase;
+
+import org.codehaus.nanning.test.TestUtils;
+
+import org.codehaus.nanning.prevayler.SoftMap;
+import org.codehaus.nanning.config.AspectSystem;
+import org.codehaus.nanning.config.FindTargetMixinAspect;
+import org.codehaus.nanning.Aspects;
+
+public class SoftMapTest extends TestCase {
+    private String smallObject;
+    private String largeObject;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        smallObject = "key";
+        largeObject = "hej";
+        for (int i = 0; i < 400; i++) {
+            largeObject += "hej";
+        }
+    }
+
+    public void testSoftValues() throws InterruptedException {
+
+        SoftMap map = SoftMap.createSoftValuesMap();
+        map.put(smallObject, largeObject);
+
+        assertEquals(largeObject, map.get(smallObject));
+        largeObject = null;
+
+        TestUtils.gc();
+
+        assertNull("Value was not removed after GC", map.get(smallObject));
+    }
+
+    public void testSoftKeys() throws InterruptedException {
+
+        SoftMap map = SoftMap.createSoftKeysMap();
+        map.put(largeObject, smallObject);
+
+        assertEquals(smallObject, map.get(largeObject));
+        largeObject = null;
+
+        TestUtils.gc();
+
+        assertNull("Value was not removed after GC", map.get(largeObject));
+    }
+
+    public void testSerializationOfSoftValues() throws IOException, ClassNotFoundException {
+
+        SoftMap map = SoftMap.createSoftValuesMap();
+        map.put(smallObject, largeObject);
+
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(data);
+        out.writeObject(map);
+        out.close();
+
+        ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(data.toByteArray()));
+        SoftMap readMap = (SoftMap) in.readObject();
+        in.close();
+        in = null;
+        out = null;
+
+        assertEquals(largeObject, readMap.get(smallObject));
+        largeObject = null;
+
+        TestUtils.gc();
+        assertNull("Value was not removed after serialization and GC", readMap.get(smallObject));
+    }
+
+    public void testSerializationOfSoftKeys() throws IOException, ClassNotFoundException {
+
+        SoftMap map = SoftMap.createSoftKeysMap();
+        map.put(largeObject, smallObject);
+
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(data);
+        out.writeObject(map);
+        out.close();
+
+        ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(data.toByteArray()));
+        SoftMap readMap = (SoftMap) in.readObject();
+        in.close();
+        in = null;
+        out = null;
+
+        assertEquals(smallObject, readMap.get(largeObject));
+        largeObject = null;
+
+        TestUtils.gc();
+        assertNull("Value was not removed after serialization and GC", readMap.get(largeObject));
+    }
+    
+    public void testSerializationWithAspects() throws IOException, ClassNotFoundException {
+        AspectSystem system = new AspectSystem();
+        Aspects.setContextAspectFactory(system);
+        system.addAspect(new FindTargetMixinAspect());
+        Object o = system.newInstance(MyObject.class);
+
+        SoftMap map = SoftMap.createSoftKeysMap();
+        map.put(o, "miffo");
+        
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(data);
+        out.writeObject(map);
+        out.close();
+
+        ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(data.toByteArray()));
+        SoftMap readMap = (SoftMap) in.readObject();
+        in.close();
+        in = null;
+        out = null;
+
+        assertEquals("miffo", readMap.get(readMap.keySet().iterator().next()));
+    }
+}
---
+++ tags/BUILD_1727/nanning/src/frameworks/remote/src/test/org/codehaus/nanning/remote/MySystem.java
@@ -0,0 +1,22 @@
+package org.codehaus.nanning.remote;
+
+import java.util.Collection;
+
+import org.prevayler.util.clock.ClockedSystem;
+
+
+/**
+ * @entity
+ */
+public interface MySystem extends ClockedSystem {
+    MyObject createMyObject();
+
+    /**
+     * @transaction
+     */
+    void setMyObject(MyObject myObject);
+
+    MyObject getMyObject();
+
+    Collection getAllObjects();
+}
