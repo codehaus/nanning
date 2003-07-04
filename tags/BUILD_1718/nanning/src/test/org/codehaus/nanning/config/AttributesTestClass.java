2b6e29dced377e3dc09a62be4189612fbcdc3e8d
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-07-04 13:05:17 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1718'.

---
+++ tags/BUILD_1718/nanning/src/test/org/codehaus/nanning/config/AttributesTestClass.java
@@ -0,0 +1,10 @@
+package org.codehaus.nanning.config;
+
+public interface AttributesTestClass {
+    /**
+     * @attribute
+     */
+    void methodWithAttribute();
+
+    void methodWithoutAttribute();
+}
