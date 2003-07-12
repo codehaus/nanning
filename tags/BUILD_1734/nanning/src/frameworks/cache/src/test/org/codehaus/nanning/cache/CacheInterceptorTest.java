9b6fcd9b745f6ededbd7212dcae68f1ff6a44fc7
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-07-12 16:48:18 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1734'.

---
+++ tags/BUILD_1734/nanning/src/frameworks/cache/src/test/org/codehaus/nanning/cache/CacheInterceptorTest.java
@@ -0,0 +1,53 @@
+package org.codehaus.nanning.cache;
+
+import org.codehaus.nanning.attribute.AbstractAttributesTest;
+import org.codehaus.nanning.AspectInstance;
+import org.codehaus.nanning.Mixin;
+import org.codehaus.nanning.cache.CacheInterceptor;
+
+import java.lang.reflect.Method;
+
+public class CacheInterceptorTest extends AbstractAttributesTest {
+    private Method someHeavyCalculationMethod;
+    private CacheInterceptor cacheInterceptor;
+    private boolean wasCalled;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        cacheInterceptor = new CacheInterceptor();
+        someHeavyCalculationMethod =
+                CacheTestCalculations.class.getMethod("someHeavyCalculation",
+                        new Class[] { double.class } );
+    }
+
+    public void testIntercept() {
+        AspectInstance aspectInstance = new AspectInstance();
+        Mixin mixin = new Mixin(CacheTestCalculations.class, null);
+        mixin.addInterceptor(someHeavyCalculationMethod, cacheInterceptor);
+        aspectInstance.addMixin(mixin);
+        mixin.setTarget(new CacheTestCalculations() {
+            public double someHeavyCalculation(double input) {
+                wasCalled = true;
+                return input;
+            }
+        });
+        CacheTestCalculations cacheTestCalculations = (CacheTestCalculations) aspectInstance.getProxy();
+
+        // non-cached call
+        assertFalse(wasCalled);
+        assertEquals(0, cacheTestCalculations.someHeavyCalculation(0), 0);
+        assertTrue(wasCalled);
+
+        // cached call
+        wasCalled = false;
+        assertEquals(0, cacheTestCalculations.someHeavyCalculation(0), 0);
+        assertFalse(wasCalled);
+
+        // flush cache and non-cached call
+        cacheInterceptor.clearCache();
+        wasCalled = false;
+        assertEquals(0, cacheTestCalculations.someHeavyCalculation(0), 0);
+        assertTrue(wasCalled);
+    }
+}
