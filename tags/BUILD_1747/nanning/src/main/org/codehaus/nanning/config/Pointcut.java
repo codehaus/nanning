9520f641c830d0fc0197fa4a34632fc44e917ce4
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-07-24 16:46:17 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1747'.

---
+++ tags/BUILD_1747/nanning/src/main/org/codehaus/nanning/config/Pointcut.java
@@ -0,0 +1,83 @@
+package org.codehaus.nanning.config;
+
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.List;
+
+import org.codehaus.nanning.AspectException;
+import org.codehaus.nanning.AspectInstance;
+import org.codehaus.nanning.MethodInterceptor;
+import org.codehaus.nanning.Mixin;
+
+public abstract class Pointcut {
+
+    /**
+     * Reuses the same interceptor on every advised method
+     * @param instance
+     * @param interceptor
+     */
+    public void advise(AspectInstance instance, MethodInterceptor interceptor) {
+        advise(instance, interceptor, null);
+    }
+
+    /**
+     * Instantiates a new interceptor for each advised method.
+     * @param instance
+     * @param interceptorClass
+     */
+    public void advise(AspectInstance instance, Class interceptorClass) {
+        advise(instance, null, interceptorClass);
+    }
+
+    public void advise(AspectInstance instance, MethodInterceptor interceptor, Class interceptorClass) {
+        List mixins = instance.getMixins();
+        for (Iterator iterator = mixins.iterator(); iterator.hasNext();) {
+            Mixin mixin = (Mixin) iterator.next();
+            Method[] methods = mixin.getAllMethods();
+            for (int i = 0; i < methods.length; i++) {
+                Method method = methods[i];
+                if (adviseMethod(instance, mixin, method)) {
+                    if (interceptor != null) {
+                        mixin.addInterceptor(method, interceptor);
+                    } else if (interceptorClass != null) {
+                        try {
+                            mixin.addInterceptor(method, (MethodInterceptor) interceptorClass.newInstance());
+                        } catch (Exception e) {
+                            throw new AspectException(e);
+                        }
+                    } else {
+                        assert false : "interceptor or class needs to be specified";
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Override this method for a pointcut that selects methods to advise.
+     *
+     * @param instance
+     * @param mixin
+     * @param method
+     * @return
+     */
+    public boolean adviseMethod(AspectInstance instance, Mixin mixin, Method method) {
+        return false;
+    }
+
+    /**
+     * Override this method for a pointcut that selects aspect-instances to introduce on.
+     *
+     * @param instance
+     * @return
+     */
+    public boolean introduceOn(AspectInstance instance) {
+        return false;
+    }
+
+    public void introduce(AspectInstance instance, Mixin mixin) {
+        if (introduceOn(instance)) {
+            instance.addMixin(mixin);
+        }
+    }
+}
