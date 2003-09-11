5fcffbb810200a24a968ed0ab0592c72f6003ad5
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-09-11 15:45:59 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_1761'.

---
+++ tags/BUILD_1761/nanning/src/frameworks/profiler/src/main/org/codehaus/nanning/profiler/ProfilerLogger.java
@@ -0,0 +1,30 @@
+package org.codehaus.nanning.profiler;
+
+import org.codehaus.nanning.Invocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class ProfilerLogger {
+    private static final Log logger = LogFactory.getLog(ProfilerLogger.class);
+
+    private static ProfilerLogger instance = new ProfilerLogger();
+
+    String lastLog;
+
+    private ProfilerLogger() {
+    }
+
+    public static ProfilerLogger getProfilerLogger() {
+        return instance;
+    }
+
+    public void log(Invocation invocation, long duration) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(invocation.getTarget().toString()).
+                append(".").append(invocation.getMethod().getName()).
+                append(": ").append(duration).append("ms");
+        lastLog = sb.toString();
+        logger.info(lastLog);
+    }
+
+}
