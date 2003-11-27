5976f7cf7b397a6939071d4efa141910e3995bf6
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-11-27 15:54:20 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_2085'.

---
+++ tags/BUILD_2085/nanning/src/frameworks/attribute/src/test/org/codehaus/nanning/attribute/Job.java
@@ -0,0 +1,24 @@
+package org.codehaus.nanning.attribute;
+
+/**
+ * @transaction required
+ */
+public class Job {
+    /**
+     * @transient true
+     */
+    private String description;
+
+    private String boss;
+
+    public void fireAllEmployees() {
+    }
+
+    /**
+     * @nanning great
+     * @param employee
+     */
+    public void hireEmployee(String reason, Employee employee) {
+    }
+
+}
