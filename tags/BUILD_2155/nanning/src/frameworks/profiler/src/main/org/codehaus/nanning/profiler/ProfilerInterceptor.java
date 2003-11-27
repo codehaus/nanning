4f78d95e66e7e1c2d60087808723ecdc545b4444
nobody <nobody@3d9f0f3f-b372-0410-8897-e56e3f393856>
2003-11-27 15:54:22 +0000

    This commit was manufactured by cvs2svn to create tag 'BUILD_2155'.

---
+++ tags/BUILD_2155/nanning/src/frameworks/profiler/src/main/org/codehaus/nanning/profiler/ProfilerInterceptor.java
@@ -0,0 +1,37 @@
+package org.codehaus.nanning.profiler;
+
+import org.codehaus.nanning.Invocation;
+import org.codehaus.nanning.MethodInterceptor;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ProfilerInterceptor implements MethodInterceptor {
+    static ThreadLocal threadLocal = new ThreadLocal();
+
+    private static long minDuration = 0;
+
+    public Object invoke(Invocation invocation) throws Throwable {
+        Map methodsStartingTime = (Map) threadLocal.get();
+        if (methodsStartingTime == null) {
+            methodsStartingTime = new HashMap();
+            threadLocal.set(methodsStartingTime);
+        }
+        Method method = invocation.getMethod();
+        methodsStartingTime.put(method, new Long(System.currentTimeMillis()));
+        Object result = invocation.invokeNext();
+        long duration = System.currentTimeMillis() - ((Long) methodsStartingTime.get(method)).longValue();
+        if (duration >= minDuration) ProfilerLogger.getProfilerLogger().log(invocation, duration);
+        return result;
+    }
+
+    public static long getMinDuration() {
+        return minDuration;
+    }
+
+    public static void setMinDuration(long l) {
+        minDuration = l;
+    }
+}
---
+++ tags/BUILD_2155/nanning/src/test/org/codehaus/nanning/config/AspectSystemTest.java
@@ -0,0 +1,76 @@
+package org.codehaus.nanning.config;
+
+import org.codehaus.nanning.*;
+import junit.framework.TestCase;
+
+public class AspectSystemTest extends TestCase {
+
+    public void test() throws NoSuchMethodException {
+        AspectSystem aspectSystem = new AspectSystem();
+        aspectSystem.addAspect(new FindTargetMixinAspect());
+        aspectSystem.addAspect(new MixinAspect(TestMixin.class, TestMixinImpl.class, P.all()));
+        aspectSystem.addAspect(new InterceptorAspect(new MockInterceptor()));
+        final NullInterceptor nullInterceptor = new NullInterceptor();
+        aspectSystem.addAspect(new InterceptorAspect(nullInterceptor));
+        final MockConstructionInterceptor constructionInterceptor = new MockConstructionInterceptor();
+
+        aspectSystem.addAspect(new Aspect() {
+            public void introduce(AspectInstance aspectInstance) {
+            }
+
+            public void advise(AspectInstance aspectInstance) {
+                aspectInstance.addConstructionInterceptor(constructionInterceptor);
+            }
+        });
+
+        Object bigMomma = aspectSystem.newInstance(Intf.class);
+        assertTrue(Aspects.isAspectObject(bigMomma));
+        assertTrue(bigMomma instanceof Intf);
+        assertTrue(bigMomma instanceof TestMixin);
+
+        assertEquals(2, Aspects.getInterceptors(bigMomma).size());
+
+        constructionInterceptor.verify();
+
+        assertSame(nullInterceptor,
+                   Aspects.getAspectInstance(bigMomma).getMixinForInterface(Intf.class).
+                   getInterceptorsForMethod(Intf.class.getMethod("call", new Class[0])).get(1));
+        MockInterceptor callInterceptor =
+                (MockInterceptor) Aspects.getAspectInstance(bigMomma).getMixinForInterface(Intf.class).
+                getInterceptorsForMethod(Intf.class.getMethod("call", new Class[0])).get(0);
+        MockInterceptor mixinCallInterceptor =
+                (MockInterceptor) Aspects.getAspectInstance(bigMomma).getMixinForInterface(TestMixin.class).
+                getInterceptorsForMethod(TestMixin.class.getMethod("mixinCall", new Class[0])).get(0);
+        ((TestMixin) bigMomma).mixinCall();
+        mixinCallInterceptor.verify();
+        ((Intf) bigMomma).call();
+        callInterceptor.verify();
+    }
+
+    public static interface IntfSub extends Intf {
+    }
+
+    public static class IntfSubImpl extends IntfImpl {
+    }
+
+    public void testInheritance() throws NoSuchMethodException {
+        AspectSystem aspectSystem = new AspectSystem();
+        aspectSystem.addAspect(new FindTargetMixinAspect());
+        aspectSystem.addAspect(new InterceptorAspect(NullInterceptor.class, InterceptorAspect.PER_METHOD));
+        Object bigMomma = aspectSystem.newInstance(IntfSub.class);
+
+        assertEquals(1,
+                     Aspects.getAspectInstance(bigMomma).getMixinForInterface(Intf.class).
+                     getInterceptorsForMethod(Intf.class.getMethod("call", new Class[0])).size());
+    }
+
+    public static interface Base {
+        public void m();
+    }
+
+    public static interface Sub extends Base {
+    }
+
+    public static interface C extends Sub, Base {
+    }
+}
